7b70b2f8580b7f1656da6a328beb6720bfdbb7ab
ZherdevMak <Zherdev.mak@gmail.com>
2022-05-19 21:59:29 +0300

    lesson5added

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,23 @@ import {Rating} from "./components/Rating";
 function App() {
     return (
         <div>
-            <input/>
-            This is APP component
-            <Accordion title={"My friends"} collapsed={true} />
-            <Rating />
+            <PageTitle title ={"This is APP component"}/>
+            <PageTitle title ={"My friends"}/>
+            Article 1
+            <Rating value={3} />
+            <Accordion title={"My Accordion"} collapsed={true} />
+            <Accordion title={"My friend"} collapsed={true} />
+            Article 2
+            <Rating value={0} />
+            <Rating value={1} />
+            <Rating value={2} />
+            <Rating value={3} />
+            <Rating value={4} />
+            <Rating value={5} />
         </div>
     );
 }
-
+function PageTitle(props: any) {
+    return <h1>{props.title}</h1>
+}
 export default App;
